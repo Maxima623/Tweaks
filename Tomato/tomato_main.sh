caca0ffddd1d1276c567ddff9168c3ad7d4c5c23
Maxima623 <33653628+Maxima623@users.noreply.github.com>
2017-12-02 16:53:26 +0800

    tomato_main.sh: Change order of download

--- Tomato/tomato_main.sh
+++ Tomato/tomato_main.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 if [ ! -e /tmp/tweaked ]; then sleep 10;
-  wget -qO- https://raw.githubusercontent.com/Maxima623/Tweaks/master/Tomato/tomato_firewall.sh | sh
   wget -qO- https://raw.githubusercontent.com/Maxima623/Tweaks/master/Tomato/tomato_wanup.sh | sh
+  wget -qO- https://raw.githubusercontent.com/Maxima623/Tweaks/master/Tomato/tomato_firewall.sh | sh
   echo > /tmp/tweaked
 fi
 
