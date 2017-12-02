e35932ccb19419dea53142663c812c27ec644e2d
Maxima623 <33653628+Maxima623@users.noreply.github.com>
2017-12-02 17:31:29 +0800

    tomato_firewall.sh: Add semicolons

--- Tomato/tomato_firewall.sh
+++ Tomato/tomato_firewall.sh
@@ -1,27 +1,27 @@
 #! /bin/sh
 
-iptables -I INPUT 3 -p udp -m state --state NEW -j DROP
-iptables -I INPUT 3 -p udp -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT
-iptables -I INPUT 3 -p udp -m connlimit --connlimit-above 64 -j DROP
-iptables -I INPUT 3 -p tcp --syn -m state --state NEW -j DROP
-iptables -I INPUT 3 -p tcp --syn -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT
-iptables -I INPUT 3 -p tcp --syn -m connlimit --connlimit-above 128 -j DROP
+iptables -I INPUT 3 -p udp -m state --state NEW -j DROP;
+iptables -I INPUT 3 -p udp -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT;
+iptables -I INPUT 3 -p udp -m connlimit --connlimit-above 64 -j DROP;
+iptables -I INPUT 3 -p tcp --syn -m state --state NEW -j DROP;
+iptables -I INPUT 3 -p tcp --syn -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT;
+iptables -I INPUT 3 -p tcp --syn -m connlimit --connlimit-above 128 -j DROP;
 
-iptables -I FORWARD 3 -p udp -m state --state NEW -j DROP
-iptables -I FORWARD 3 -p udp -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT
-iptables -I FORWARD 3 -p udp -m connlimit --connlimit-above 64 -j DROP
-iptables -I FORWARD 3 -p tcp --syn -m state --state NEW -j DROP
-iptables -I FORWARD 3 -p tcp --syn -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT
-iptables -I FORWARD 3 -p tcp --syn -m connlimit --connlimit-above 128 -j DROP
+iptables -I FORWARD 3 -p udp -m state --state NEW -j DROP;
+iptables -I FORWARD 3 -p udp -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT;
+iptables -I FORWARD 3 -p udp -m connlimit --connlimit-above 64 -j DROP;
+iptables -I FORWARD 3 -p tcp --syn -m state --state NEW -j DROP;
+iptables -I FORWARD 3 -p tcp --syn -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT;
+iptables -I FORWARD 3 -p tcp --syn -m connlimit --connlimit-above 128 -j DROP;
 
-iptables -I OUTPUT -m state --state INVALID -j DROP
+iptables -I OUTPUT -m state --state INVALID -j DROP;
 
-iptables -t mangle -I PREROUTING -p tcp --tcp-flags ALL ALL -j DROP
-iptables -t mangle -I PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
-iptables -t mangle -I PREROUTING -p tcp -m state --state NEW -m tcpmss ! --mss 536:65535 -j DROP
-iptables -t mangle -I PREROUTING -p tcp ! --syn -m state --state NEW -j DROP
-iptables -t mangle -I PREROUTING -m state --state INVALID -j DROP
-iptables -t mangle -I PREROUTING -f -j DROP
-iptables -t mangle -I PREROUTING -p icmp -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags ALL ALL -j DROP;
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags ALL NONE -j DROP;
+iptables -t mangle -I PREROUTING -p tcp -m state --state NEW -m tcpmss ! --mss 536:65535 -j DROP;
+iptables -t mangle -I PREROUTING -p tcp ! --syn -m state --state NEW -j DROP;
+iptables -t mangle -I PREROUTING -m state --state INVALID -j DROP;
+iptables -t mangle -I PREROUTING -f -j DROP;
+iptables -t mangle -I PREROUTING -p icmp -j DROP;
 
 exit 0
