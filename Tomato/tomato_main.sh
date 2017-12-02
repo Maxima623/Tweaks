114d84a2f9d1b49b02a6d49f08a648dd97aa08fe
Maxima623 <33653628+Maxima623@users.noreply.github.com>
2017-12-02 16:43:18 +0800

    tomato_main.sh: Update

--- Tomato/tomato_main.sh
+++ Tomato/tomato_main.sh
@@ -3,7 +3,6 @@
 if [ ! -e /tmp/tweaked ]; then sleep 10;
   wget -qO- https://raw.githubusercontent.com/Maxima623/Tweaks/master/Tomato/tomato_firewall.sh | sh
   wget -qO- https://raw.githubusercontent.com/Maxima623/Tweaks/master/Tomato/tomato_wanup.sh | sh
-  
   echo > /tmp/tweaked
 fi
 
