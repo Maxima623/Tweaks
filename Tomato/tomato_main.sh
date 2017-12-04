850d16f5063aca61a6b797b82fb9f857db0a3532
Maxima623 <33653628+Maxima623@users.noreply.github.com>
2017-12-04 13:30:03 +0800

    tomato_main.sh: Trash it. Doesn't work out as intended

--- Tomato/tomato_main.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-if [ ! -e /tmp/tweaked ]; then 
-  wget -qO- https://raw.githubusercontent.com/Maxima623/Tweaks/master/Tomato/tomato_wanup.sh | sh
-  wget -qO- https://raw.githubusercontent.com/Maxima623/Tweaks/master/Tomato/tomato_firewall.sh | sh
-  echo > /tmp/tweaked
-fi
-
-exit 0
