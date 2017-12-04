480c580c56fd7c044cf7dcbdd4dc05b41ef0589b
Maxima623 <33653628+Maxima623@users.noreply.github.com>
2017-12-04 13:04:28 +0800

    tomato_firewall.sh: Updates

--- Tomato/tomato_firewall.sh
+++ Tomato/tomato_firewall.sh
@@ -1,27 +1,29 @@
 #! /bin/sh
 
-iptables -I INPUT 3 -p udp -m state --state NEW -j DROP;
-iptables -I INPUT 3 -p udp -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT;
-iptables -I INPUT 3 -p udp -m connlimit --connlimit-above 64 -j DROP;
-iptables -I INPUT 3 -p tcp --syn -m state --state NEW -j DROP;
-iptables -I INPUT 3 -p tcp --syn -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT;
-iptables -I INPUT 3 -p tcp --syn -m connlimit --connlimit-above 128 -j DROP;
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags FIN,RST FIN,RST -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags FIN,ACK FIN -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags ACK,URG URG -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags ACK,FIN FIN -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags ACK,PSH PSH -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags ALL ALL -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags ALL NONE -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags ALL SYN,FIN,PSH,URG -j DROP
+iptables -t mangle -I PREROUTING -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
 
-iptables -I FORWARD 3 -p udp -m state --state NEW -j DROP;
-iptables -I FORWARD 3 -p udp -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT;
-iptables -I FORWARD 3 -p udp -m connlimit --connlimit-above 64 -j DROP;
-iptables -I FORWARD 3 -p tcp --syn -m state --state NEW -j DROP;
-iptables -I FORWARD 3 -p tcp --syn -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT;
-iptables -I FORWARD 3 -p tcp --syn -m connlimit --connlimit-above 128 -j DROP;
+iptables -t mangle -I PREROUTING -p tcp -m state --state NEW -m tcpmss ! --mss 536:65535 -j DROP
+iptables -t mangle -I PREROUTING -p tcp ! --syn -m state --state NEW -j DROP
+iptables -t mangle -I PREROUTING -m state --state INVALID -j DROP
+iptables -t mangle -I PREROUTING -f -j DROP
+iptables -t mangle -I PREROUTING -p icmp -j DROP
 
-iptables -I OUTPUT -m state --state INVALID -j DROP;
-
-iptables -t mangle -I PREROUTING -p tcp --tcp-flags ALL ALL -j DROP;
-iptables -t mangle -I PREROUTING -p tcp --tcp-flags ALL NONE -j DROP;
-iptables -t mangle -I PREROUTING -p tcp -m state --state NEW -m tcpmss ! --mss 536:65535 -j DROP;
-iptables -t mangle -I PREROUTING -p tcp ! --syn -m state --state NEW -j DROP;
-iptables -t mangle -I PREROUTING -m state --state INVALID -j DROP;
-iptables -t mangle -I PREROUTING -f -j DROP;
-iptables -t mangle -I PREROUTING -p icmp -j DROP;
+iptables -t nat -I PREROUTING -p udp -m state --state NEW -j DROP
+iptables -t nat -I PREROUTING -p udp -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT
+iptables -t nat -I PREROUTING -p udp -m multiport ! --dports 53,80,443 -m connlimit --connlimit-above 128 -j DROP
+iptables -t nat -I PREROUTING -p tcp --syn -m state --state NEW -j DROP
+iptables -t nat -I PREROUTING -p tcp --syn -m state --state NEW -m limit --limit 16/second --limit-burst 16 -j ACCEPT
+iptables -t nat -I PREROUTING -p tcp --syn -m multiport ! --dports 53,80,443 -m connlimit --connlimit-above 128 -j DROP
 
 exit 0
