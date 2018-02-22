683fa97007e18abd9f09e7ec7399edd749b543bd
Maxima623 <33653628+Maxima623@users.noreply.github.com>
2018-02-22 11:15:15 +0800

    adblock.sh: Restart dnsmasq after downloading hosts

--- Tomato/adblock.sh
+++ Tomato/adblock.sh
@@ -12,5 +12,6 @@ HOSTS="
       "
 
 wget -q -O - $HOSTS | grep .0.0. | sed -e '2,$s/127.0.0.1/0.0.0.0/g' -e 's/[[:space:]]*#.*$//' | grep -v localhost | tr ' ' '\t' | tr -s '\t' | tr -d '\015' | sort -u | awk 'NF > 0' > xhosts
+killall -1 dnsmasq
 
 exit 0
