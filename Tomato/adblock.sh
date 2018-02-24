1cc7cf93ec8d53fafbbed25d6473b89b1b6cb411
Maxima623 <33653628+Maxima623@users.noreply.github.com>
2018-02-24 17:23:17 +0800

    adblock.sh: Try to use Steven Black's Unified Hosts and hpHosts Ads & Tracking Servers list

--- Tomato/adblock.sh
+++ Tomato/adblock.sh
@@ -1,11 +1,8 @@
 #! /bin/sh
 
 HOSTS="
-        ""https://adaway.org/hosts.txt""
-        ""http://someonewhocares.org/hosts/zero/hosts""
-        ""http://www.malwaredomainlist.com/hostslist/hosts.txt""
-        ""http://winhelp2002.mvps.org/hosts.txt""
-        ""https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext&useip=0.0.0.0""
+        ""https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts""
+        ""https://hosts-file.net/ad_servers.txt""
       "
 
 wget -q -O - $HOSTS | grep .0.0. | sed -e '2,$s/127.0.0.1/0.0.0.0/g' -e 's/[[:space:]]*#.*$//' | grep -v localhost | tr ' ' '\t' | tr -s '\t' | tr -d '\015' | sort -u | awk 'NF > 0' > xhosts
